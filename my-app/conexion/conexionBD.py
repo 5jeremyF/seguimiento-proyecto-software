e0288608e8ade8ff710673cc1e932fed9b25035f
ajacomeh <126479641+ajacomeh@users.noreply.github.com>
2026-05-12 17:16:20 -0500

    update

--- my-app/conexion/conexionBD.py
+++ my-app/conexion/conexionBD.py
@@ -1,24 +1,18 @@
-
-
 # Importando Libreria mysql.connector para conectar Python con MySQL
 import mysql.connector
 
 
 def connectionBD():
     try:
-        # connection = mysql.connector.connect(
         connection = mysql.connector.connect(
             host="localhost",
             user="root",
             passwd="",
             database="crud_python",
-            charset='utf8mb4',
-            collation='utf8mb4_unicode_ci',
-            raise_on_warnings=True
-
+            charset='utf8mb4'
         )
+
         if connection.is_connected():
-            # print("Conexión exitosa a la BD")
             return connection
 
     except mysql.connector.Error as error:
